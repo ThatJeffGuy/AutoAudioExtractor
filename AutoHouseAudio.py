36b4dd15d5d84856029419aa1310aa9e15cf6335
ThatJeffGuy <49243483+ThatJeffGuy@users.noreply.github.com>
2024-06-09 12:36:19 -0400

    diarize bugtesting

--- AutoHouseAudio.py
+++ AutoHouseAudio.py
@@ -44,8 +44,8 @@ def create_and_activate_venv():
     logging.info(f"Python executable path: {python_executable}")
 
     # Install necessary packages in the new virtual environment
+    subprocess.check_call([python_executable, '-m', 'pip', 'install', 'torch==2.0.1+cu117', 'torchaudio==2.0.1+cu117', 'torchvision==0.15.2+cu117', '--extra-index-url', 'https://download.pytorch.org/whl/cu117'])
     subprocess.check_call([python_executable, '-m', 'pip', 'install', 'pyannote.audio[cuda]'])
-    subprocess.check_call([python_executable, '-m', 'pip', 'install', 'torch==2.0.0+cu117', 'torchaudio==2.0.0+cu117', 'torchvision==0.15.1+cu117', '--extra-index-url', 'https://download.pytorch.org/whl/cu117'])
     subprocess.check_call([python_executable, '-m', 'pip', 'install', 'speechbrain'])
 
     # Check for dependency conflicts
